3159979708f247b82fa050e5fee4660270d01ea5
manana06 <150535521+manana06@users.noreply.github.com>
2024-01-05 02:16:34 +0200

    Create app01.py

---
+++ app01.py
@@ -0,0 +1,8 @@
+import csv
+
+def all_clerks():
+  name = input("Give a name: ")
+  address = input( "Give an address: ")
+  items = input( "Give the order: ")
+  date = input("Give the date: "  )
+  total = input("Give a total cost: ")
