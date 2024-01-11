d8f8233c6d81fe0bf77332b20f4f77e1e2e79e2c
manana06 <150535521+manana06@users.noreply.github.com>
2024-01-11 17:42:08 +0200

    Update app01.py

--- app01.py
+++ app01.py
@@ -25,3 +25,22 @@ def employees_role():
   items = input( "Give the order: ")
   date = input("Give the date: "  )
   total = input("Give a total cost: ")
+
+def get_empoyees():
+  file = open("empoyees.csv")
+  reader = csv.DirectReader(file)
+  users = {}
+  for row in reader:
+    users[row['username']] = row
+  file.close()
+  return users
+
+def get_customers():
+  file = open("customers.csv")
+    reader = csv.DictReader(file)
+    customers = {}
+    for row in reader:
+        customers[row['name']] = row 
+    file.close()
+    return customers 
+  
